ff20fcd148b441094f4be2928e19277cc2ca1297
561067 <153899946+561067@users.noreply.github.com>
2023-12-16 10:31:02 +0200

    Add files via upload

---
+++ Lesson_1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int number = 5;
+        System.out.println(number);
+        int numb = 5;
+        int num = 4;
+        System.out.println( numb + num );
+
+    }
+}
---
+++ Lesson_1/src/ScannerSempl.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ScannerSempl {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        String str = "Hello" + name;
+        System.out.println(str);
+
+
+
+    }
+}
+
